613ec8dda5e6332d014b67ea152a6040fcece0e6
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-08 10:09:57 +0900

    Initial Commit

---
+++ HaiQuri/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HaiQuri
+//
+//  Created by 櫻井絵理香 on 2024/03/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HaiQuri/HaiQuriApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaiQuriApp.swift
+//  HaiQuri
+//
+//  Created by 櫻井絵理香 on 2024/03/08.
+//
+
+import SwiftUI
+
+@main
+struct HaiQuriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
