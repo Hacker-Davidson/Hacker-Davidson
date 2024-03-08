ac780b8ed324f9952daab4e1be6016621dd0cfb6
Sawaki Shuto <swkshuto0208@icloud.com>
2024-03-08 11:17:49 +0900

    編集
    Logic.swift編集

--- HaiQuri/Logic.swift
+++ HaiQuri/Logic.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+
+struct sacredPlace: Identifiable {
+    let id: String
+    let title: String
+    let placeName: String
+    let adress: String
+    let latitude: Double
+    let longitude: Double
+}
+
+class Logic: ObservableObject {
+    @Published var animeTitle: String = ""
+    @Published var animeSubTitle: String = ""
+    let csvContents: [String] = []
+    let 
+
+
+
+}
