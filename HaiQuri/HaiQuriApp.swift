54cebad9be135205a5a6582ed14b366d6dd7c3d0
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-08 11:03:52 +0900

    topbarの色変更

--- HaiQuri/HaiQuriApp.swift
+++ HaiQuri/HaiQuriApp.swift
@@ -9,6 +9,24 @@ import SwiftUI
 
 @main
 struct HaiQuriApp: App {
+    init() {
+        //NavigationBarの設定
+               let appearance = UINavigationBarAppearance()
+               appearance.configureWithOpaqueBackground()
+               appearance.backgroundColor = .blue  // ナビゲーションバーの背景色
+               appearance.titleTextAttributes = [.foregroundColor: UIColor.white]  // タイトルの色
+               appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]  // 大きなタイトルの色
+
+               // ナビゲーションバーとスクロールビューの間の透明な境界線を削除
+               appearance.shadowColor = nil
+
+               //Navigationbarの標準の外観にする
+               UINavigationBar.appearance().standardAppearance = appearance
+               //スクロールしたとき
+               UINavigationBar.appearance().scrollEdgeAppearance = appearance
+               //横向きの設定
+               UINavigationBar.appearance().compactAppearance = appearance // For iPhone small navigation bar in landscape.
+       }
     var body: some Scene {
         WindowGroup {
             ContentView()
