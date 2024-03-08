50933e718443572eef67b0b3f0dd6a2042ae8197
orukahairuka <sakuraierika03301109@gmail.com>
2024-03-08 12:22:17 +0900

    extensionColor設定

---
+++ HaiQuri/ColorExtension.swift
@@ -0,0 +1,8 @@
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBackColor = Color("BackgrondColor")
+    static let customButtonColor = Color("ButtonColor")
+    static let customTopBar = Color("TopBarColor")
+}
--- HaiQuri/ContentView.swift
+++ HaiQuri/ContentView.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        FloatButton()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+struct FloatButton: View {
+    var body: some View {
+        HStack { // --- 2
+            NavigationLink(destination: ContentView()) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "pencil")
+                    Text("掲示板")
+                    Spacer()
+                }
+            }
+            .foregroundColor(.white)
+            .font(.system(size: 24))
+
+        }
+        .frame(width: 130, height: 60)
+        .background(Color.orange)
+        .cornerRadius(30.0)
+        .shadow(color: .gray, radius: 3, x: 3, y: 3)
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 16.0, trailing: 16.0)) // --- 5
+        .offset(x: 124, y: -90)
+    }
 }
