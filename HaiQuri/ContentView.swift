5f51e4a4a26d92d02a2e4263e656daeb46ce6304
Sawaki Shuto <swkshuto0208@icloud.com>
2024-03-09 21:20:57 +0900

    変更
    レイアウト変更

--- HaiQuri/ContentView.swift
+++ HaiQuri/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var logic = Logic()
+    @State private var title: String = ""
     var body: some View {
         NavigationView {
             ZStack {  // --- 1
+                MapView(logics: logic)
+                SearchBar(logic: logic)
+
                 FloatButton()
-///////////////////////////////////////////RouteFinishButtonは道案内するときにだけ表示させたい
+                ///////////////////////////////////////////RouteFinishButtonは道案内するときにだけ表示させたい
                 RouteFinishButton()
+                
             }
             .toolbar {
                 customToolbarItems
@@ -94,12 +100,12 @@ extension ContentView {
                     Image(systemName: "list.bullet")
                 }
             }
-
+            
         }
     }
-
+    
 }
 
-    #Preview {
-        ContentView()
-    }
+#Preview {
+    ContentView()
+}
