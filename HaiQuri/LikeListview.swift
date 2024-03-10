e9bc8f05da639371dc9199832f5f06cfba5116a3
Sawaki Shuto <swkshuto0208@icloud.com>
2024-03-11 06:20:11 +0900

    追加
    好きなやつのいちらん

--- HaiQuri/LikeListview.swift
+++ HaiQuri/LikeListview.swift
@@ -6,13 +6,28 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct LikeListview: View {
+    var logic: Logic
+    //マップ情報取得
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Entity.latitude, ascending: true)],
+        animation: .default)
+    private var entityList: FetchedResults<Entity>
+    //追加
+    @Environment(\.managedObjectContext) private var viewContext
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        List(entityList, id: \.placeName) { entity in
+            VStack(alignment: .leading){
+                Text(entity.placeName ?? "")
+                    .fontWeight(.bold)
+                Text(entity.title ?? "")
+                Text(entity.adress ?? "")
+                    .font(.caption)
+            }
 
-#Preview {
-    LikeListview()
+
+        }
+    }
 }
